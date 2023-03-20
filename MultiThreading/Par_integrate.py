8062832621438d8c66a46837ee76639af7b1945a
Dr. Emil A. Gazazyan <emil@quopt.net>
2023-03-20 11:13:08 +0400

    push: par_integrate

---
+++ MultiThreading/Par_integrate.py
@@ -0,0 +1,37 @@
+import threading
+import scipy.integrate as spi
+
+def integrate(f, a, b, num_threads=2):
+    """
+    Computes the definite integral of f(x) over the interval [a, b]
+    using the scipy.integrate.quad function and parallelism with threading.
+    """
+    # Define a function to be run by each thread
+    def worker(i, subinterval):
+        integrals[i] = spi.quad(f, *subinterval)[0]
+
+    # Split the interval [a, b] into num_threads subintervals
+    intervals = [(a + (b - a) / num_threads * i, a + (b - a) / num_threads * (i + 1))
+                 for i in range(num_threads)]
+
+    # Create a list to store the results of each thread's integration
+    integrals = [0] * num_threads
+
+    # Create a list to store the threads
+    threads = []
+
+    # Create and start a thread for each subinterval
+    for i, subinterval in enumerate(intervals):
+        t = threading.Thread(target=worker, args=(i, subinterval))
+        threads.append(t)
+        t.start()
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+    # Return the sum of all thread integrals
+    return sum(integrals)
+
+result = integrate(lambda x: x**2, 0, 1, num_threads=4)
+print(result)
